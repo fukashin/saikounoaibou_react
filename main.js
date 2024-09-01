a70306b278c9c4e3b8b7882ff9d69ef19537fc5a
fukashin <55280398+fukashin@users.noreply.github.com>
2024-09-01 14:57:00 +0900

    add 画面表示位置をサブディスプレイの右上に設定

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
 
 // ホットリロードの設定ここから
-const path = require('path');
 require('electron-reload')(path.join(__dirname, './src'), {
-  // Electronの実行ファイルのパスを明示的に指定
   electron: path.join(__dirname, './node_modules/electron/dist/electron.exe'),
   ignored: /node_modules|[/\\]\./
 });
-
 // ホットリロードの設定ここまで
 
 function createWindow() {
+  // すべてのディスプレイの情報を取得
+  const displays = screen.getAllDisplays();
+
+  // サブディスプレイの右上に表示するためのディスプレイを選択
+  // ここでは2つ目のディスプレイをサブディスプレイと仮定
+  const externalDisplay = displays.find((display) => display.bounds.x !== 0 || display.bounds.y !== 0);
+
+  let x = 0, y = 0;
+  
+  if (externalDisplay) {
+    // サブディスプレイの右上の座標を計算
+    x = externalDisplay.bounds.x + externalDisplay.bounds.width - 800; // ウィンドウ幅を800と仮定
+    y = externalDisplay.bounds.y; // 上部はそのまま
+  }
+
   let mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    x: x, // 計算されたX座標を設定
+    y: y, // 計算されたY座標を設定
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -22,7 +37,7 @@ function createWindow() {
   });
 
   mainWindow.loadURL('http://localhost:3000');
-//   mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null;
