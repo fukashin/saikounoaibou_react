cca0e7bf5c4b7853c92d1381e4221f4a57ca9be1
fukashin <55280398+fukashin@users.noreply.github.com>
2024-09-01 14:26:48 +0900

    add ホットリロードを追加

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+// ホットリロードの設定
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
 function createWindow() {
-  let mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'), // preloadスクリプトがある場合
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
-  // React開発サーバーをロード
-  mainWindow.loadURL('http://localhost:3000');  // Reactの開発サーバーURL
-
-  // 開発者ツールを開く（必要に応じてコメントアウト）
+  mainWindow.loadURL('http://localhost:3000');
 //   mainWindow.webContents.openDevTools();
 
-  // ウィンドウが閉じられた時の処理
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
 }
 
-// Electronが初期化されたときにウィンドウを作成
 app.on('ready', createWindow);
 
-// 全てのウィンドウが閉じられた時の処理
 app.on('window-all-closed', function () {
-  // macOS では Cmd + Q でアプリを終了するまでアプリを終了させない
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', function () {
-  // macOS で dock アイコンがクリックされ、他にウィンドウが開いていない場合に再作成する
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
